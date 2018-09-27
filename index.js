d8bdf1c3ad88f295a28cfbd6d2f1fd713dd668dc
Jayon Kim <jpkim921@gmail.com>
2018-09-27 00:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+  const hq = 42;
+  return Math.abs(hq - blocks);
+}
+
+function distanceFromHqInFeet(blocks){
+  return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(dist1, dist2 ) {
+  return Math.abs(distanceFromHqInFeet(dist1) - distanceFromHqInFeet(dist2));
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  return distance;
+  let cost;
+
+  // distance < 0 ? cost = 0 : cost = distance * 0.02
+
+  switch (distance) {
+    case distance > 2500:
+      cost = "cannot travel that far";
+      break;
+    case distance > 2000:
+      cost = 25;
+      break;
+    case distance < 400:
+      cost = 0;
+      break;
+    default:
+      cost = distance * 0.02;
+      break;
+  }
+
+  return cost;
+}
